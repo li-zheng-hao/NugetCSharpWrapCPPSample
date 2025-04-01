dfbacd8d318db821288f21e44140e8d867a189c5
LiZhengHao <1263212577@qq.com>
2025-04-01 09:09:05 +0800

    Add files via upload

---
+++ NugetConsole/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NugetLib;
+var res=Class1.Add(1, 2);
+Console.WriteLine("Hello, World!"+res.ToString());
---
+++ NugetLib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace NugetLib
+{
+    public class Class1
+    {
+        [DllImport("DemoLib",EntryPoint = "add")]
+        public static extern int Add(int a, int b);
+    }
+}
+
